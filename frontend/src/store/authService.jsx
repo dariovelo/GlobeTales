725502f6f04292e1bd5c0271bb0d4c77ab853949
Dario Velo <57144932+velodario@users.noreply.github.com>
2024-08-28 01:23:42 +0300

    Update authService.jsx

--- frontend/src/store/authService.jsx
+++ frontend/src/store/authService.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/user/";
+const API_URL = "https://globetales-backend.onrender.com/api/user/";
 
 const register = async (userData) => {
   const response = await axios.post(API_URL + "register", userData);
